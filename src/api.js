5c8e52d4e257e6bbca172acf4523ce47b9e8203c
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-07-01 09:54:03 +0100

    modify api.js

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 const getUserScore = async () => {
-  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/166tll3CLk1QzxL0tLc0/scores');
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/g9lrTWdCVB9CVLhwmVMJR7W/scores');
 
   const { result } = await response.json();
   return result;
 };
 
 const setUserScore = async (userScore) => {
-  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/166tll3CLk1QzxL0tLc0/scores',
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/g9lrTWdCVB9CVLhwmVMJR7W/scores',
     {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
